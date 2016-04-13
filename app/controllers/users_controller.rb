b87afeac3e5ef469e8531d041795cddadd13b6e8
Greed1702 <bluediamond1702@gmail.com>
2016-04-13 13:35:41 +0700

    Add sign in upon sigh up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			sessions[:user_id] = @user.id
 			flash[:success] = "Welcome to the Ruby on Rails-based Blog #{@user.username}"
-			redirect_to articles_path
+			redirect_to user_path(@user)
 		else
 			render "new"
 		end
